202b907d313a0a5e9abc4737ad9ebf41033d5159
Navita Ramasar <navitaramasar14@gmail.com>
2023-08-08 19:03:27 -0400

    loop lab

--- index.js
+++ index.js
@@ -1,1 +1,44 @@
-// Code your solutions in this file
+// Code your solutions in this filea
+
+const namesArr = ['Guadalupe', 'Ollie', 'Aki']
+
+function writeCards(names) {
+    const cardNames = []
+    let i = 0
+    while (i < names.length) {
+       cardNames.push(`Thank you, ${names[i]}, for the wonderful surprise gift!`)
+       i++
+    }
+    return cardNames;
+}
+
+writeCards(namesArr);
+
+
+
+
+// number = 
+// function countDown(number){
+//     let i = number
+//     while (i > 0) {
+//     console.log(i)
+//     i--
+//     }
+// countDown(number)
+// }
+
+number = 10
+function countDown(number){
+    let i = number;
+     while (i + 1 > 0) {
+    console.log(i);
+    i--;
+  }
+}
+
+
+
+
+
+
+
